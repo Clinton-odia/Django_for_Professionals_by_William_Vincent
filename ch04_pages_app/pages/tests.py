20dd15b44ca0686e5725a14b8d84fe66cdd7a3bc
Clinton Odia <odiamehiclinton@gmail.com>
2025-07-30 05:42:50 +0100

    Test pages
    add testcase for templates, homepage urls

--- ch04_pages_app/pages/tests.py
+++ ch04_pages_app/pages/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class HomepageTest(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template_used(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "<h1>Homepage</h1>")
