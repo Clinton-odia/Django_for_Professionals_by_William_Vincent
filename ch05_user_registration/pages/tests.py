9c62333f7e53a34f401d6a5346e813f3cd08cab4
Clinton Odia <odiamehiclinton@gmail.com>
2025-07-30 09:11:28 +0100

    Refactor: move my signup test code to account test
    add signup form and view test

--- ch05_user_registration/pages/tests.py
+++ ch05_user_registration/pages/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from .views import HomePageView
-from django.contrib.auth import get_user_model
 
 
 # Create your tests here.
@@ -24,15 +23,3 @@ class HomepageTest(TestCase):
     def test_homepage_url_resolve_homepageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
-
-
-class SignupPageTests(TestCase):
-    def setUp(self):
-        url = reverse("signup")
-        self.response = self.client.get(url)
-
-    def test_signup_template(self):
-        self.assertTemplateUsed(self.response, "registration/signup.html")
-        self.assertEqual(self.response.status_code, 200)
-        self.assertContains(self.response, "Sign Up")
-        self.assertNotContains(self.response, "I am looking for you")
