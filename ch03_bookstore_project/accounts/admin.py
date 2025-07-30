3bd4c801c55e140095f09b17609a8198608798cb
Clinton Odia <odiamehiclinton@gmail.com>
2025-07-30 03:56:14 +0100

    Configure custom user admin

--- ch03_bookstore_project/accounts/admin.py
+++ ch03_bookstore_project/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 # Register your models here.
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = [
+        "email",
+        "username",
+    ]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
