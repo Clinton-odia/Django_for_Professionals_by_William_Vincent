2a1cd54c4212f6707bc58946a3f0aec4da27a3c5
Clinton Odia <odiamehiclinton@gmail.com>
2025-07-30 05:49:16 +0100

    refactor: use setup to keep it DRY

--- ch04_pages_app/pages/tests.py
+++ ch04_pages_app/pages/tests.py
@@ -4,16 +4,17 @@ from django.urls import reverse
 
 # Create your tests here.
 class HomepageTest(TestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
     def test_home_page_status_code(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_home_page_url(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template_used(self):
-        response = self.client.get(reverse("home"))
-        self.assertTemplateUsed(response, "base.html")
-        self.assertTemplateUsed(response, "home.html")
-        self.assertContains(response, "<h1>Homepage</h1>")
+        self.assertTemplateUsed(self.response, "base.html")
+        self.assertTemplateUsed(self.response, "home.html")
+        self.assertContains(self.response, "<h1>Homepage</h1>")
