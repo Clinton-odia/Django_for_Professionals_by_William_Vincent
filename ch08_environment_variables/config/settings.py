e631ccda607df0966d1dc4c70473b4215bd5e953
Clinton Odia <odiamehiclinton@gmail.com>
2025-08-13 01:03:32 +0100

    Chapter 8 completed
    Note: add dependencies for db to work

--- ch08_environment_variables/config/settings.py
+++ ch08_environment_variables/config/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -93,9 +93,12 @@ DATABASES = {
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "db",
-        "POST": 5432,
+        "PORT": 5432,
     }
 }
+# DATABASES = {
+#     "default": env.dj_db_url("DATABASE_URL", default="postgres://postgres@db/postgres")
+# }
 
 
 # Password validation
