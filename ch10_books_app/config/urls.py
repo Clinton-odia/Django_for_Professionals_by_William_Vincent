d921e3f03f14da139ca602644c6e141b36ad2b4f
Clinton Odia <odiamehiclinton@gmail.com>
2025-08-14 12:33:01 +0100

    Feat: add book list view, urls and template

--- ch10_books_app/config/urls.py
+++ ch10_books_app/config/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     # User management
     path("accounts/", include("allauth.urls")),
     # local apps
+    path("books/", include("books.urls")),
     path("", include("pages.urls")),
 ]
