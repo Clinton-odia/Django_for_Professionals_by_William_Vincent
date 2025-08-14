1cdc2e882381c5bb331357bb72f180d3b8655eda
Clinton Odia <odiamehiclinton@gmail.com>
2025-08-14 14:41:05 +0100

    Feat: review template added

--- ch11_reviews_app/books/admin.py
+++ ch11_reviews_app/books/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Review
 # Register your models here.
 
 
+class ReviewAdmin(admin.TabularInline):
+    model = Review
+
+
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewAdmin,
+    ]
     list_display = ("title", "author", "price")
 
 
